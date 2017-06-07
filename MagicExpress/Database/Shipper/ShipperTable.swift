271ababd0ea7df941e7b4759ae14b9d5f28106d8
Shvier <daulvi@hotmail.com>
2017-06-07 19:45:28 +0800

    Add INSERT and DELETE for ShipperTable

--- MagicExpress/Database/Shipper/ShipperTable.swift
+++ MagicExpress/Database/Shipper/ShipperTable.swift
@@ -9,18 +9,44 @@
 class ShipperTable: BaseTable {
     
     static func insertShipper(shipper: ShipperModel) {
-        
+        if !(DatabaseManager.sharedInstance.database?.open())! {
+            DatabaseManager.sharedInstance.database = nil
+            return
+        }
+        if shipper.logisticCode == nil || shipper.shipperCode == nil || shipper.updateTime == nil {
+            return
+        }
+        do {
+            try DatabaseManager.sharedInstance.database?.executeUpdate("INSERT INTO Shipper (logistic_code, shipper_code, update_time)" + "VALUES (?, ?, ?)", values: [shipper.logisticCode!, shipper.shipperCode!, shipper.updateTime!])
+        } catch (let error) {
+            print("insert error: \(error)")
+        }
+        DatabaseManager.sharedInstance.database?.close()
     }
     
-    static func updateShipper(shipper: ShipperModel) {
-        
+    static func deleteShipper(logisticCode: String!, shipperCode: String!) {
+        if !(DatabaseManager.sharedInstance.database?.open())! {
+            DatabaseManager.sharedInstance.database = nil
+            return
+        }
+        do {
+            try DatabaseManager.sharedInstance.database?.executeUpdate("DELETE FROM Shipper WHERE logistic_code = ? AND shipper_code = ?", values: [logisticCode, shipperCode])
+            // delete from Trace Table
+            // TODO
+        } catch (let error) {
+            print("insert error: \(error)")
+        }
+        DatabaseManager.sharedInstance.database?.close()
     }
     
-    static func deleteShipper(shipper: ShipperModel) {
-        
-    }
-    
-    static func getShipper(logisticCoe: String, shipperCode: String) -> ShipperModel {
+    static func getShipper(logisticCode: String, shipperCode: String) -> ShipperModel {
+        if !(DatabaseManager.sharedInstance.database?.open())! {
+            DatabaseManager.sharedInstance.database = nil
+            return ShipperModel()
+        }
+        do {
+            
+        }
         return ShipperModel()
     }
 
