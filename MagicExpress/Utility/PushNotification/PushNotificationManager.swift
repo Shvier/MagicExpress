177c843b999278c90d833776f8139d3e9ea06adf
Shvier <daulvi@hotmail.com>
2017-06-20 15:20:26 +0800

    Add PushNotificationManager

---
+++ MagicExpress/Utility/PushNotification/PushNotificationManager.swift
@@ -0,0 +1,13 @@
+//
+//  PushNotificationManager.swift
+//  MagicExpress
+//
+//  Created by shvier on 20/06/2017.
+//  Copyright © 2017 Shvier. All rights reserved.
+//
+
+import Cocoa
+
+class PushNotificationManager: NSObject {
+
+}
